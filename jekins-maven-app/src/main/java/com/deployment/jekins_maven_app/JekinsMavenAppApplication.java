91a6e6d9dad52e998c82cdfc876104f68a115af0
Aman Jain <amanj0318@gmail.com>
2024-09-26 10:57:20 +0530

    Added Project File

---
+++ jekins-maven-app/src/main/java/com/deployment/jekins_maven_app/JekinsMavenAppApplication.java
@@ -0,0 +1,13 @@
+package com.deployment.jekins_maven_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JekinsMavenAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JekinsMavenAppApplication.class, args);
+	}
+
+}
---
+++ jekins-maven-app/src/test/java/com/deployment/jekins_maven_app/JekinsMavenAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deployment.jekins_maven_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JekinsMavenAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
